18b00e8035a4790446ad217a1c400050343a9cdd
raju-ahammad <web.rajuahammad@gmail.com>
2020-07-26 12:50:17 +0600

    secound commit

---
+++ src/Components/video.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class video extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="ui center aligned header">Hello Youtube App</h1>
+            </div>
+        )
+    }
+}
+
+export default video
