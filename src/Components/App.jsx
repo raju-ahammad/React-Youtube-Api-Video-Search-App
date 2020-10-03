e01e6d050bcb62f19153386dffad1d559c760374
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-04 00:02:45 +0600

    setup bootstrap

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,25 +1,13 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import youtube from '../Api/youtube'
+import React from 'react';
+import SearchBar from './SearchBar';
 
 
 
 class App extends React.Component {
-    onMyinputSubmit = myInput => {
-        youtube.get("/search", {
-            params: {
-                q: myInput,
-                part: "snippet",
-                maxResults: 5,
-                key: "AIzaSyCaTdTsZ0wKDK_ijbf5zPLf6tpeXsXmECE"
-              }
-        })
-
-    }
     render() {
         return (
-            <div className='ui container'>
-                <SearchBar onFormSubmit={this.onMyinputSubmit}/>
+            <div className='container text-danger'>
+                <SearchBar />
             </div>
         );
     }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,30 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {
-        myInput: ""
-    }
-    onChangeHandler = event => {
-        this.setState({myInput: event.target.value})
-    }
-    onFormSubmitHanler = event => {
-        event.preventDefault()
-        this.props.onFormSubmit(this.state.myInput);
-    }
-
+    
     render() {
         return(
-            <div className= "search-bar ui segment" style={{ marginTop: "10px" }}>
-                <h1 className="ui center aligned header">Youtube Video Search App</h1>
-                <form action="" onSubmit={ this.onFormSubmitHanler } className="ui form">
-                    <div className="field">
-                        <label htmlFor="">Video Search</label>
-                        <input type="text"
-                        value = {this.state.myInput}
-                        onChange = {this.onChangeHandler}
-                        />
-                    </div>
-                </form>
+            <div>
+                Hello
             </div>
         );
     }
